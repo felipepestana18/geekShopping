8aa243c8b54aa3371bd2b262adf6768bb6395dbf
AzureAD\FELIPEDASILVAPESTANA <felipe-pe27@hotmail.com>
2022-09-24 22:36:52 -0300

    add: classe product e as migration

--- Solution/GeekShooping.ProductAPI/Program.cs
+++ Solution/GeekShooping.ProductAPI/Program.cs
@@ -1,6 +1,14 @@
+using GeekShooping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+// Configuração do MySql
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 15))));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
