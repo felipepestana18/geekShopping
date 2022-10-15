e20b9fa66bcc0fd1c6f97eb3114ef02a6c3dd9c0
AzureAD\FELIPEDASILVAPESTANA <felipe-pe27@hotmail.com>
2022-10-14 21:49:32 -0300

    add: project identity server

---
+++ Solution/GeekShooping.IdentityServer/Model/Context/MySqlContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.IdentityServer.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+    }
+}
